90aa468c990ad99237dd29942ec35021cc1d4c54
MrR3set <bairon.pazrobles@gmail.com>
2020-02-21 10:43:11 -0800

    mvp and stretch

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -1,26 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import {
   Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle, Button
 } from 'reactstrap';
 
 const Example = (props) => {
-    console.log(props.data)
-    const [films,setFilms] = useState([]);
+    let films = [];
+    const [planet,setPlanet] = useState([]);
+    // const [species, setSpecies] = useState();
 
-
-
-
+    //get films
+    useEffect(()=>{
+        props.data.films.forEach(element => {
+            axios.get(element).then(res=>{  
+                films.push(res.data.title)
+            }).catch(error=>console.log(error));
+        });   
+    },[])
+    
+    //get planet
+    useEffect(()=>{
+        axios.get(props.data.homeworld).then(res=>{  
+            setPlanet(res.data.name)
+         }).catch(error=>console.log(error));
+    },[])
+    
   return (
     <div className="card-wrapper">
       <Card>
         <CardBody>
           <CardTitle>{props.data.name}</CardTitle>
-          <CardSubtitle>From PLANET</CardSubtitle>
+  <CardSubtitle>From {planet}</CardSubtitle>
   <CardText>Gender: {props.data.gender}<br></br>DoB: {props.data.birth_year}<br></br>Height: {props.data.height}<br></br>Mass {props.data.mass} Units<br></br> <br></br>Hair color: {props.data.hair_color}<br></br>Skin color: {props.data.fair}<br></br>Eye color: {props.data.eye_color}</CardText>
-          <CardText>Display all the films</CardText>
-          <CardText>More things</CardText>
-          <CardText>Display all the films</CardText>
+  <CardText>Films: {films}</CardText>
+ 
         </CardBody>
       </Card>
     </div>
--- starwars/src/components/CardsPage.js
+++ starwars/src/components/CardsPage.js
@@ -1,6 +1,7 @@
 import React,{useEffect, useState} from 'react';
 import axios from "axios";
 import Card from "./Card"
+import Pagination from "./Pagination"
 import './StarWars.css';
 
 
@@ -8,19 +9,43 @@ import './StarWars.css';
 const App = () => {
     const [data, setData] = useState([]);
 
+    const [page, setPage] = useState(1);
+    
+    const pHandlerNext = () => {
+        if(page>=data.count){
+        }else{
+            setPage(page+1);
+        }
+    }
+    const pHandlerPrev = () => {
+        if(page<=1){
+        }else{
+            setPage(page-1);
+        }
+    }
+
+    console.log(page);
+
+
     useEffect(()=>{
-    axios.get("https://swapi.co/api/people").then(res=>{
+    axios.get(`https://swapi.co/api/people/?page=${page}&format=json`).then(res=>{
         setData(res.data.results);
     }).catch(error=>console.log(error));
-    },[])
-
+    },[page])
+    
   return (
-    <div className="cards-wrapper">
+
+    <div>
+        <div className="pages">
+            <Pagination next={pHandlerNext} prev={pHandlerPrev}></Pagination>
+        </div>
+        <div className="cards-wrapper">
         {
             data.map(elem => {
                 return <Card data={elem}></Card>
             })
         }
+        </div>
     </div>
   );
 }
